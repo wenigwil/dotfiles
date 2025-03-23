35154dece9b5c13ae73ef8da6d13db0069699699
wenigwil <williwenig@gmx.de>
2025-03-23 21:21:26 +0100

    Rice it up...

--- qtile/dot-config/qtile/config.py
+++ qtile/dot-config/qtile/config.py
@@ -3,44 +3,30 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
+
+
 mod = "mod4"
 terminal = guess_terminal()
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    Key([mod, "shift"], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod, "shift"], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod, "shift"], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod, "shift"], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "w", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], "h", lazy.layout.swap_left(), desc="Swap the window to be on top of the stack."),
+    Key([mod], "j", lazy.layout.down(), desc="Focus down."),
+    Key([mod], "k", lazy.layout.up(), desc="Focus up."),
+    Key([mod], "l", lazy.layout.shuffle_down(), desc="Shuffle the window down."),
+    Key([mod], "n", lazy.layout.reset(), desc="Reset all window sizes"),
+    Key([mod], "w", lazy.layout.left(), desc="Focus the top of the stack."),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "Return", lazy.spawn("alacritty"), desc="Launch terminal"),
+    Key([mod], "o", lazy.layout.maximize(), desc="Toggle between min and max."),
     Key([mod], "f", lazy.spawn("firefox"), desc="Launch Firefox"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "Return", lazy.spawn("alacritty"), desc="Launch terminal"),
+    Key([mod, "shift"], "h", lazy.layout.grow_main(), desc="Grow window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shrink_main(), desc="Grow window to the right"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
 
     # Multimedia Keys
     Key([], "XF86AudioLowerVolume", lazy.spawn('pactl set-sink-volume $(pactl get-default-sink) -5%',shell=True)),
@@ -54,52 +40,46 @@ keys = [
 ]
 
 
-# Initialize all "desktops"
-groups = [Group(i) for i in "123456789"]
 
-for i in groups:
+
+# Initialize all "desktops"
+groups = [Group(i) for i in ['TeX','Res','Ink','Cod','Sys','Misc']]
+
+for index, i in enumerate(groups):
     keys.extend(
         [
             # mod + group number = switch to group
             Key(
                 [mod],
-                i.name,
+                str(index+1),
                 lazy.group[i.name].toscreen(),
                 desc=f"Switch to group {i.name}",
             ),
             # mod + shift + group number = switch to & move focused window to group
             Key(
                 [mod, "shift"],
-                i.name,
+                str(index+1),
                 lazy.window.togroup(i.name, switch_group=True),
                 desc=f"Switch to & move focused window to group {i.name}",
-            ),
+            )])
             # Or, use below if you prefer not to switch to that group.
-            # # mod + shift + group number = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
-
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.MonadTall(
+        align = 0,
+        auto_maximize = True,
+        border_focus = '#ff9e64',
+        border_normal = '#3d59a1',
+        border_width = 4,
+        ratio = 2/3,
+        new_client_position = "bottom",
+        min_secondary_size = 130,
+        ),
+    layout.TreeTab(),
 ]
 
 widget_defaults = dict(
     font="JetBrains Mono Bold",
-    fontsize=12,
+    fontsize=13,
     padding=2,
 )
 extension_defaults = widget_defaults.copy()
@@ -108,15 +88,35 @@ screens = [
     Screen(
         bottom=bar.Bar(
             [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Clock(format="%d.%m.%Y %I:%M %p"),
+                widget.GroupBox(
+                    fontsize=14,
+                    block_highlight_text_color="ff9e64",
+                    center_aligned=True,
+                    disable_drag=True,
+                    highlight_color='292e42',
+                    highlight_method='line',
+                    this_current_screen_border='ff9e64',
+                    this_screen_border='ff9e64',
+                    use_mouse_wheel=False,
+                    ),
+                widget.Prompt(
+                    cursor=True,
+                    cursorblink=0,
+                    fmt='<span text_transform="uppercase" rise="7pt">{}</span>',
+                    cursor_color='#292e42',
+                    ),
+                widget.Spacer(length=bar.STRETCH),
+                widget.CurrentLayout(fmt='<span text_transform="uppercase" rise="7pt">{}</span>'),
+                widget.Spacer(length=bar.STRETCH),
+                widget.Clock(
+                    format="%A KW-%W %d.%m.%Y %H:%M:%S",
+                    fmt='<span rise="7pt">{}</span>'
+                    ),
+                widget.Spacer(length=5),
             ],
+            # Size/thickness of the bar
             24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            background = "#292e42",
         ),
     ),
 ]
@@ -164,6 +164,12 @@ wl_xcursor_size = 24
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
 # mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
 # this string if your java app doesn't work correctly. We may as well just lie
 # and say that we're a working one by default.
 #
