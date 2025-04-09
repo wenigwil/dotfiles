ac59935d34bf8abe3a5cd315f9b4148f5d170356
wenigwil <williwenig@gmx.de>
2025-04-09 13:53:08 +0200

    Added vivfy forced light mode config

--- nvim/dot-config/nvim/lua/config/plugins/markdown-preview.lua
+++ nvim/dot-config/nvim/lua/config/plugins/markdown-preview.lua
@@ -1,20 +1,20 @@
 return {
-    --     "iamcco/markdown-preview.nvim",
-    --     build = "cd app && npm install",
-    --     init = function()
-    --         vim.g.mkdp_filetypes = { "markdown" }
-    --     end,
-    --     config = function()
-    --         vim.keymap.set("n", "<Leader>mp", "<Plug>MarkdownPreview", { desc = "Markdown Preview" })
-    --         vim.g.mkdp_theme = 'light'
-    --         -- Unfortunately vimscript has to be introduced to configure this
-    --         vim.cmd([[do FileType]])
-    --         vim.cmd([[
-    --          function OpenMarkdownPreview (url)
-    --             let cmd = "firefox --new-window " . shellescape(a:url) . " &"
-    --             silent call system(cmd)
-    --          endfunction
-    --       ]])
-    --         vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
-    --     end,
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    config = function()
+        vim.keymap.set("n", "<Leader>mp", "<Plug>MarkdownPreview", { desc = "Markdown Preview" })
+        vim.g.mkdp_theme = 'light'
+        -- Unfortunately vimscript has to be introduced to configure this
+        vim.cmd([[do FileType]])
+        vim.cmd([[
+             function OpenMarkdownPreview (url)
+                let cmd = "firefox --new-window " . shellescape(a:url) . " &"
+                silent call system(cmd)
+             endfunction
+          ]])
+        vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+    end,
 }
