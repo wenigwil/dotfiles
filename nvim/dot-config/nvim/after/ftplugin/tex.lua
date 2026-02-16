4d72acdde02b8ac9294a2315729a9933ad950dfb
wenigwil <github@wenigwil.com>
2026-02-16 09:34:57 +0100

    Added bash completion that was not installed automatically

--- nvim/dot-config/nvim/after/ftplugin/tex.lua
+++ nvim/dot-config/nvim/after/ftplugin/tex.lua
@@ -16,5 +16,14 @@ vim.keymap.set({ 'n' }, "<F2>", function()
     vim.cmd(":VimtexView")
 end)
 
+vim.opt_local.shiftwidth = 4
+vim.opt_local.formatoptions:append('t')
+vim.opt_local.formatoptions:append('w')
+vim.opt_local.formatoptions:append('a')
+vim.opt_local.formatoptions:append('n')
+vim.opt_local.textwidth = 85
+
+vim.b.miniindentscope_disable = true
+
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "en_us"
