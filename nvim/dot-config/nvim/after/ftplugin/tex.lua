901bd5ef98281c918f9e2ae85a2ca89f395761b3
wenigwil <github@wenigwil.com>
2026-06-17 12:40:12 +0200

    Fixed the codeheading function in my .bashrc to work with auto-formatting conventions

--- nvim/dot-config/nvim/after/ftplugin/tex.lua
+++ nvim/dot-config/nvim/after/ftplugin/tex.lua
@@ -2,6 +2,13 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.textwidth = 85
 
 
+vim.opt_local.formatoptions:append('o')
+vim.opt_local.formatoptions:append('r')
+vim.opt_local.formatoptions:append('j')
+vim.opt_local.formatoptions:append('a')
+vim.opt_local.formatoptions:append('w')
+
+
 vim.opt_local.spelllang = "en"
 vim.opt_local.spell = true
 vim.opt_local.relativenumber = false
